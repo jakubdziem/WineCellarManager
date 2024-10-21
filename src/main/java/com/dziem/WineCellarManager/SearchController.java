a03806e112ae6408eb192d81caf6d5114b1d60fb
Jakub Dziem <jakub.dziem9@gmail.com>
2024-10-21 19:30:34 +0200

    Add search controller and site. Add images for home page.

---
+++ src/main/java/com/dziem/WineCellarManager/SearchController.java
@@ -0,0 +1,12 @@
+package com.dziem.WineCellarManager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+    @GetMapping("/search")
+    public String getRegisterPage() {
+        return "search";
+    }
+}
