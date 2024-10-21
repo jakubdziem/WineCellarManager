f24916e11597cfadeff99bc4ffceb262400cdbbc
Jakub Dziem <jakub.dziem9@gmail.com>
2024-10-21 19:31:24 +0200

    Update home page and controller. Add more images for home page.

--- src/main/java/com/dziem/WineCellarManager/HomePageController.java
+++ src/main/java/com/dziem/WineCellarManager/HomePageController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String getHomePage() {
-        return "index";
+        return "home";
     }
 
 
