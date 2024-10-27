5d3b76a0e572b6b49ad6f045549282ef743a1e96
Jakub Dziem <jakub.dziem9@gmail.com>
2024-10-27 16:44:00 +0100

    Add controller method to send wines grouped by wineType. Add showing of the result of this controller method mentioned earlier.

--- src/main/java/com/dziem/WineCellarManager/controller/CollarController.java
+++ src/main/java/com/dziem/WineCellarManager/controller/CollarController.java
@@ -1,12 +1,32 @@
 package com.dziem.WineCellarManager.controller;
 
+import com.dziem.WineCellarManager.model.Wine;
+import com.dziem.WineCellarManager.model.WineType;
+import com.dziem.WineCellarManager.repository.WineRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
+@RequiredArgsConstructor
 public class CollarController {
+    private final WineRepository wineRepository;
     @GetMapping("/collar")
     public String getCollarPage() {
         return "collar";
+    }
+//    @ModelAttribute("wines")
+//    public List<Wine> getExampleWines() {
+//        System.out.println(wineRepository.findAll().stream().limit(10).toList());
+//        return wineRepository.findAll().stream().limit(10).toList();
+//    }
+    @ModelAttribute("wines")
+    public Map<WineType, List<Wine>> groupWinesByType() {
+        return wineRepository.findAll().stream().skip(717).limit(10).collect(Collectors.groupingBy(Wine::getWineType));
     }
 }
